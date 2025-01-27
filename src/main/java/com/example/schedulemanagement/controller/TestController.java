8b0f6e32e4aec4397c17f8a463ec2e1564228062
euuns <dmsu2@naver.com>
2025-01-27 15:35:40 +0900

    Feat: Add Lv.1-create, read
    일정을 생성하고 조회하는 기능 작성
    [일정 생성] 작성자, 비밀번호, 제목, 내용 입력
    [일정 조회] 작성자, 제목, 내용, 자가성일 조회
    
    일정 작성 후 확인을 누르면 작성한 내용을 조회하도록 설정

---
+++ src/main/java/com/example/schedulemanagement/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.example.schedulemanagement.controller;
+
+import com.example.schedulemanagement.Entity.Schedule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/schedule")
+@RequiredArgsConstructor
+public class TestController {
+
+    private Schedule schedule = new Schedule();
+
+
+    // 입력한 내용 출력
+    @GetMapping("/get")
+    public String getSchedule(Model model) {
+        model.addAttribute("schedule", schedule);
+        return "check-schedule";
+    }
+
+
+    // 정보 입력
+    @GetMapping()
+    public String getInputPage(Model model) {
+        model.addAttribute("schedule", schedule);
+        return "add-schedule";
+    }
+
+    @PostMapping()
+    public String addSchedule(@ModelAttribute Schedule schedule) {
+        this.schedule = schedule;
+        return "redirect:/schedule/get";
+    }
+
+}
