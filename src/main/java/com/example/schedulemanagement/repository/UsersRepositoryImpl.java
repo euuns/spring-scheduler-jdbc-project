775eea7584d775d9a507ff0651397a67d29dab26
euuns <dmsu2@naver.com>
2025-01-31 19:25:11 +0900

    Feat: Add Users - Service, Repository

---
+++ src/main/java/com/example/schedulemanagement/repository/UsersRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.schedulemanagement.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsersRepositoryImpl implements UsersRepository{
+}
---
+++ src/main/java/com/example/schedulemanagement/service/UsersServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.schedulemanagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService{
+}
